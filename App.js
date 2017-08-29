278de4b7e59972fa344aafbf9fe1293ff5db6d99
Andres <ballerlikemike23@gmail.com>
2017-08-29 01:06:55 -0400

    Styles compontents working

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Container } from './styles/grid';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Text>Simba</Text>
+      </Container>
+    );
+  }
+}
